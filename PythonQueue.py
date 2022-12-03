a1c7a435738e986cbb6925aaa4736cc2711a6368
JasonG016 <114097282+JasonG016@users.noreply.github.com>
2022-12-03 12:24:52 -0500

    Queue Simulation

---
+++ PythonQueue.py
@@ -0,0 +1,13 @@
+names = []
+
+for x in range(10):
+    acceptedName = input("Enter name: ")
+    names.append(acceptedName)
+
+print(names)
+
+for x in range(10):
+    print(names[0])
+    names.pop(0)
+
+print(names)
